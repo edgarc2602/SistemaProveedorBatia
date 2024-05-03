a9dd9578d662caa8a73756b1d22b1d379a99cc1c
Edgar Corona Esquivel <edgarc@grupobatia.com.mx>
2024-05-03 13:14:45 -0600

    Hash password

--- SistemaVentasBatia/Services/UsuarioService.cs
+++ SistemaVentasBatia/Services/UsuarioService.cs
@@ -8,7 +8,9 @@ using SistemaVentasBatia.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace SistemaVentasBatia.Services
@@ -57,6 +59,7 @@ namespace SistemaVentasBatia.Services
 
         public async Task<UsuarioDTO> Login(AccesoDTO dto)
         {
+            dto.Contrasena = Encriptar(dto.Contrasena);
             UsuarioDTO usu;
             try
             {
@@ -77,6 +80,29 @@ namespace SistemaVentasBatia.Services
             }
 
             return usu;
+        }
+
+        public static string Encriptar(string pass)
+        {
+            // Convertir la contraseña a bytes UTF-8
+            byte[] bytes = Encoding.UTF8.GetBytes(pass);
+
+            // Crear un objeto SHA256 para calcular el hash
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                // Calcular el hash de la contraseña
+                byte[] hashBytes = sha256.ComputeHash(bytes);
+
+                // Convertir el hash a una cadena hexadecimal
+                StringBuilder builder = new StringBuilder();
+                foreach (byte b in hashBytes)
+                {
+                    builder.Append(b.ToString("x2"));
+                }
+
+                // Devolver el hash en formato hexadecimal
+                return builder.ToString();
+            }
         }
 
         public  Task<GraficaListadoAnio[]> ObtenerListadosAnio(int anio, int idProveedor)
