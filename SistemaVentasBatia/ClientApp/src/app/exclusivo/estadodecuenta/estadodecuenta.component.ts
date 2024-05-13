45da402c6d0ebe174f6895dcc600dccaf49e9b0e
Edgar Corona Esquivel <edgarc@grupobatia.com.mx>
2024-05-13 14:25:46 -0600

    totales estado de cuenta, ajuste tablas

--- SistemaVentasBatia/ClientApp/src/app/exclusivo/estadodecuenta/estadodecuenta.component.ts
+++ SistemaVentasBatia/ClientApp/src/app/exclusivo/estadodecuenta/estadodecuenta.component.ts
@@ -25,6 +25,14 @@ export class EstadoDeCuentaComponent {
     listaEvaluaciones: ListaEvaluacionProveedor = {
         evaluacion: [], idEvaluacionProveedor: 0,idProveedor: 0, idStatus: 0, fechaEvaluacion: '', numeroContrato: '', promedio: 0, textoPromedio: '', idUsuario: 0
     }
+    totalMontoFactura: number = 0;
+    totalPagos: number = 0;
+    totalSaldoActual: number = 0;
+    totalCorriente: number = 0;
+    totalPrimerMes: number = 0;
+    totalSegundoMes: number = 0;
+    totalTercerMes: number = 0;
+    totalCuartoMesOMas: number= 0
 
 
     constructor(@Inject('BASE_URL') private url: string, private http: HttpClient, public user: StoreUser) {
@@ -35,8 +43,30 @@ export class EstadoDeCuentaComponent {
     ngOnInit() {
         this.http.get<ListadoEstadoDeCuenta>(`${this.url}api/cuenta/getestadodecuenta/${this.user.idProveedor}/${this.model.pagina}`).subscribe(response => {
             this.model = response;
+            this.limpiarTotales();
+            this.model.estadosDeCuenta.forEach(estado => {
+                this.totalMontoFactura += estado.total;
+                this.totalPagos += estado.pago;
+                this.totalSaldoActual += estado.saldo;
+                this.totalCorriente += estado.corriente;
+                this.totalPrimerMes += estado.mes1;
+                this.totalSegundoMes += estado.mes2;
+                this.totalTercerMes += estado.mes3;
+                this.totalCuartoMesOMas += estado.mes4;
+            })
+
         })
         
+    }
+    limpiarTotales() {
+        this.totalMontoFactura = 0;
+        this.totalPagos = 0;
+        this.totalSaldoActual = 0;
+        this.totalCorriente = 0;
+        this.totalPrimerMes = 0;
+        this.totalSegundoMes = 0;
+        this.totalTercerMes = 0;
+        this.totalCuartoMesOMas = 0;
     }
 
     muevePagina(event) {
